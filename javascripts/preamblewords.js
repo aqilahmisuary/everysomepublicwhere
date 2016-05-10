351ef6e55adc4b4ec90ee46d3bb29b1699e1d845
Aqilah Misuary <qila@live.com.sg>
2016-05-10 19:53:10 +0800

    slides

---
+++ javascripts/preamblewords.js
@@ -0,0 +1,20 @@
+var myName = "/\_____-----_/\/\/\/\___ -----~~~~=====+++++++++++__-----_/\/\///__-----_/\/\////";
+
+//var red = [0, 100, 200];
+var letterColors = ["black"];
+
+drawName(myName, letterColors);
+
+if(20 < 3)
+{
+    bubbleShape = 'square';
+   
+}
+else
+{
+    bubbleShape = 'square';
+ 
+}
+
+bounceBubbles();
+
